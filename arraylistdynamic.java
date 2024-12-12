7b4d803f3706f18966b84ac087da7971993bb97c
likitha <likkilikitha214@gmail.com>
2024-12-12 14:37:59 +0530

    Add files via upload

---
+++ arraylistdynamic.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class arraylistdynamic
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        ArrayList<Integer> a=new ArrayList<>();
+        for(int i=0;i<n;i++)
+        {
+            int s=sc.nextInt();
+            a.add(s);
+        }
+        for(int i=0;i<a.size();i++)
+        {
+            System.out.print(a.get(i)+" ");
+        }
+    }
+}
