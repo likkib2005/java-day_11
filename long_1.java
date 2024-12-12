703edb57dfc2d482693399a50974889d89c04862
likitha <likkilikitha214@gmail.com>
2024-12-12 14:20:41 +0530

    Add files via upload

---
+++ long_1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class long_1
+{
+    public static void main(String args[])
+    {
+        int ans=90;
+        Scanner sc=new Scanner(System.in);
+        long s=sc.nextLong();
+        System.out.println(ans);
+        System.out.println(s);
+        sc.close();
+    }
+}
