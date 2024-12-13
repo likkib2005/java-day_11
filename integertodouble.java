57d19e6ed2b293ae0b318512c4538c7adc7fef97
likitha <likkilikitha214@gmail.com>
2024-12-13 10:39:45 +0530

    Add files via upload

---
+++ integertodouble.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class integertodouble
+{
+    public static void main(String args[])
+    {
+        Integer a=Integer.valueOf(0);
+        System.out.println(a);
+        Double s=Double.valueOf(89.0);
+        System.out.println(s);
+    }
+}
